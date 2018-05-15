7abe0c465629a3e80d07c991056c5060dd50a806
cheng <zhongkai.cheng@gmail.com>
2018-05-15 15:33:26 +0900

    add index.html

--- webserver/app.py
+++ webserver/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, abort, send_file, redirect
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, abort, send_file, redirect,session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
 import os,sys
+from threading import Lock
 
 from urls import urls_blueprint
 
@@ -9,6 +11,9 @@ app.config['SECRET_KEY'] = 'secret!'
 app.register_blueprint(urls_blueprint)
 
 socketio = SocketIO(app)
+
+thread = None
+thread_lock = Lock()
 
 @app.route('/', defaults={'req_path': ''})
 @app.route('/<path:req_path>')
@@ -38,27 +43,96 @@ def dir_listing(req_path):
     # Show directory contents
     files = []
     for x in os.listdir(abs_path):
-        if os.path.isdir(abs_path + x):  #isdirの代わりにisfileを使う
+        if os.path.isdir(abs_path + x):
             files.append(x+"/")
         elif not x.endswith('.py'):
             files.append(x)
     return render_template('files.html', files=files)
+    
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(10)
+        count += 1
+        socketio.emit('my_response',
+                      {'data': 'Server generated event', 'count': count},
+                      namespace='/test')
 
-@socketio.on('my event', namespace='/test')
+@socketio.on('my_event', namespace='/test')
 def test_message(message):
-    emit('my response', {'data': message['data']})
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
 
-@socketio.on('my broadcast event', namespace='/test')
-def test_message2(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+
+@socketio.on('leave', namespace='/test')
+def leave(message):
+    leave_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+
+@socketio.on('close_room', namespace='/test')
+def close(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
+                         'count': session['receive_count']},
+         room=message['room'])
+    close_room(message['room'])
+
+
+@socketio.on('my_room_event', namespace='/test')
+def send_room_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         room=message['room'])
+
+
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
+
+
+@socketio.on('my_ping', namespace='/test')
+def ping_pong():
+    emit('my_pong')
+
 
 @socketio.on('connect', namespace='/test')
 def test_connect():
-    emit('my response', {'data': 'Connected'})
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
-    print('Client disconnected')
-
+    print('Client disconnected', request.sid)
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0")
